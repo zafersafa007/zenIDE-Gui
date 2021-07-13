2486b4fc39a84a874dcc7fa62438323d89564372
Thomas Weber <weber_t@mailbox.org>
2021-07-12 20:22:44 -0500

    Update bad embed alert

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -47,7 +47,8 @@ import styles from './interface.css';
 
 if (window.parent !== window) {
     // eslint-disable-next-line no-alert
-    alert('This page is embedding TurboWarp in a way that is unsupported and will cease to function in the near future. Please read https://github.com/TurboWarp/scratch-gui/wiki/Embedding');
+    alert('This page is embedding TurboWarp in a way that is unsupported and will cease to function in the near future. Please read https://docs.turbowarp.org/embedding');
+    throw new Error('Invalid embed');
 }
 
 let announcement = null;
