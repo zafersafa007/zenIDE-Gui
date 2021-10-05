9ae97a41bc7a3aaa05a58b30bb6e5e03512977b0
Thomas Weber <weber_t@mailbox.org>
2021-10-04 21:02:10 -0500

    Fix service worker cache IDs
    A bit more work probably needs to be done here, but this should fix a big bug in the SW rewrite (EAGER_ASSETS_NAME never changing)

--- src/playground/generate-service-worker-plugin.js
+++ src/playground/generate-service-worker-plugin.js
@@ -60,14 +60,15 @@ class TWGenerateServiceWorkerPlugin {
                     !name.startsWith('js/library-')
                 ) lazyAssets.push(name);
             }
+            const id = hash(allAssetNames);
             const workerFile = compilation.getAsset(SW_NAME);
             const workerSource = workerFile.source.source().toString();
             const newSource = workerSource
                 .replace('__EAGER_ASSETS__', JSON.stringify(eagerAssets))
                 .replace('__HTML_ASSETS__', JSON.stringify(htmlAssets))
-                .replace('__EAGER_ASSETS_NAME__', JSON.stringify(`tw-eager-${hash(eagerAssets)}`))
+                .replace('__EAGER_ASSETS_NAME__', JSON.stringify(`tw-eager-${id}`))
                 .replace('__LAZY_ASSETS__', JSON.stringify(lazyAssets))
-                .replace('__LAZY_ASSETS_NAME__', JSON.stringify(`tw-lazy-${hash(lazyAssets)}`));
+                .replace('__LAZY_ASSETS_NAME__', JSON.stringify(`tw-lazy-${id}`));
             compilation.updateAsset(SW_NAME, new RawSource(newSource));
         });
     }
