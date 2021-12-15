c52775582623d8d69603f3ebc28e83aef0beee7d
Thomas Weber <muffin@muffin.ink>
2021-12-15 16:00:01 -0600

    Update addons

--- src/addons/addons/tw-remove-backpack/remove-backpack.js
+++ src/addons/addons/tw-remove-backpack/remove-backpack.js
@@ -1,4 +1,4 @@
-export default async function () {
+export default async function ({ addon }) {
   const resize = () => window.dispatchEvent(new Event("resize"));
   addon.self.addEventListener('disabled', resize);
   addon.self.addEventListener('reenabled', resize);
