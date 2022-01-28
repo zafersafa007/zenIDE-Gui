dd91b0e9b4cca515da6f724bb381540a55a27408
Thomas Weber <muffin@muffin.ink>
2022-01-27 21:31:00 -0600

    Optimize computeChunkedRMS

--- src/lib/audio/audio-util.js
+++ src/lib/audio/audio-util.js
@@ -2,25 +2,28 @@ import WavEncoder from 'wav-encoder';
 
 const SOUND_BYTE_LIMIT = 10 * 1000 * 1000; // 10mb
 
-const computeRMS = function (samples, scaling = 0.55) {
-    if (samples.length === 0) return 0;
+const _computeRMS = function (samples, start, end, scaling = 0.55) {
+    const length = end - start;
+    if (length === 0) return 0;
     // Calculate RMS, adapted from https://github.com/Tonejs/Tone.js/blob/master/Tone/component/Meter.js#L88
     let sum = 0;
-    for (let i = 0; i < samples.length; i++) {
+    for (let i = start; i < end; i++) {
         const sample = samples[i];
-        sum += Math.pow(sample, 2);
+        sum += sample ** 2;
     }
-    const rms = Math.sqrt(sum / samples.length);
+    const rms = Math.sqrt(sum / length);
     const val = rms / scaling;
     return Math.sqrt(val);
 };
+
+const computeRMS = (samples, scaling) => _computeRMS(samples, 0, samples.length, scaling);
 
 const computeChunkedRMS = function (samples, chunkSize = 1024) {
     const sampleCount = samples.length;
     const chunkLevels = [];
     for (let i = 0; i < sampleCount; i += chunkSize) {
         const maxIndex = Math.min(sampleCount, i + chunkSize);
-        chunkLevels.push(computeRMS(samples.slice(i, maxIndex)));
+        chunkLevels.push(_computeRMS(samples, i, maxIndex));
     }
     return chunkLevels;
 };
