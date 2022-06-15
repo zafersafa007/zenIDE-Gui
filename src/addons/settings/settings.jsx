047df60e11c7629bfbc295c761bf8971779d02cc
Thomas Weber <muffin@muffin.ink>
2022-06-15 17:49:54 -0500

    Update addon feedback link
    I'm going to try this for a bit and we'll see what, if anything, happens

--- src/addons/settings/settings.jsx
+++ src/addons/settings/settings.jsx
@@ -1052,7 +1052,7 @@ class AddonSettingsComponent extends React.Component {
                             />
                         </div>
                         <a
-                            href="https://scratch.mit.edu/users/World_Languages/#comments"
+                            href="https://scratch.mit.edu/users/GarboMuffin/#comments"
                             target="_blank"
                             rel="noreferrer"
                             className={styles.feedbackButtonOuter}
