859c536bc8486941470e926752ae2ec5108ebf53
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2022-10-19 18:01:50 -0500

    Update green-flag.jsx

--- src/components/green-flag/green-flag.jsx
+++ src/components/green-flag/green-flag.jsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import greenFlagIcon from './blueflag.png';
+import greenFlagIcon from './icon--green-flag.svg';
 import styles from './green-flag.css';
 
 const GreenFlagComponent = function (props) {
