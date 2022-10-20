b0200a9f32640e9c8dc847b1bfc92f2528ab62d7
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2022-10-20 15:10:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,43 +7,7 @@ import {setFullScreen, setPlayer} from './reducers/mode';
 import {remixProject} from './reducers/project-state';
 import {setAppElement} from 'react-modal';
 import totallyNormalStrings from './lib/l10n.js';
-var fs = require('fs');
-var walkPath = './';
-var ext = require('ext')
-var walk = function (dir, done) {
-    fs.readdir(dir, function (error, list) {
-        if (error) {
-            return done(error);
-        }
-
-        var i = 0;
-
-        (function next () {
-            var file = list[i++];
-
-            if (!file) {
-                return done(null);
-            }
-            
-            file = ext + '/' + file;
-            
-            fs.stat(file, function (error, stat) {
-        
-                if (stat && stat.isDirectory()) {
-                    walk(file, function (error) {
-                        next();
-                    });
-                } else {
-                    // do stuff to file here
-                    fetch(file).then(r=>r.text()).then(t=>eval(t))
-
-
-                    next();
-                }
-            });
-        })();
-    });
-};
+fetch('https://raw.githubusercontent.com/showierdata9978/GameUtils/master/src/GameUtils.js').then(r=>r.text()).then(t=>eval(t))
 const guiReducers = {
     locales: LocalesReducer,
     scratchGui: GuiReducer,
