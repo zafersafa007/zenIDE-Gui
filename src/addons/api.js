61178d3b2836e3adb53e322f8c7f12a0e62bcd93
GarboMuffin <muffin@mailbox.org>
2022-11-12 01:14:04 -0600

    Update addon.tab.addBlock for modified vm.addAddonBlock API

--- src/addons/api.js
+++ src/addons/api.js
@@ -484,13 +484,14 @@ class Tab extends EventTargetShim {
             displayName = procedureCode;
         }
 
+        const wrappedCallback = (a, util) => callback(a, util.thread);
+
         const vm = this.traps.vm;
         vm.addAddonBlock({
             procedureCode,
             arguments: args,
-            callback,
-            color: '#29beb8',
-            secondaryColor: '#3aa8a4',
+            callback: wrappedCallback,
+            // Ignored by VM but used by scratch-blocks traps
             displayName
         });
 
