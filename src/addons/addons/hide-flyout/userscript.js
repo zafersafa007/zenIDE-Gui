1f3a9e878f8d5bb3e00510a08e274a7eb335229e
GarboMuffin <muffin@mailbox.org>
2022-12-30 13:48:22 -0600

    addons: Update assorted bug fixes from upstream

--- src/addons/addons/hide-flyout/userscript.js
+++ src/addons/addons/hide-flyout/userscript.js
@@ -90,6 +90,12 @@ export default async function ({ addon, console, msg }) {
     }, speed * 1000);
   }
 
+  const updateIsFullScreen = () => {
+    const isFullScreen = addon.tab.redux.state.scratchGui.mode.isFullScreen;
+    document.documentElement.classList.toggle("sa-hide-flyout-not-fullscreen", !isFullScreen);
+  };
+  updateIsFullScreen();
+
   let didOneTimeSetup = false;
   function doOneTimeSetup() {
     if (didOneTimeSetup) {
@@ -114,6 +120,9 @@ export default async function ({ addon, console, msg }) {
           }
           break;
         }
+        case "scratch-gui/mode/SET_FULL_SCREEN":
+          updateIsFullScreen();
+          break;
       }
     });
 
