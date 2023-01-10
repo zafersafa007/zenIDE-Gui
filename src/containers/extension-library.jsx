8715f31bc5a31f660fe6c26019d01dfeab1e2ca2
GarboMuffin <muffin@mailbox.org>
2023-01-09 18:42:25 -0600

    Don't load URL extensions again if already loaded

--- src/containers/extension-library.jsx
+++ src/containers/extension-library.jsx
@@ -58,7 +58,7 @@ class ExtensionLibrary extends React.PureComponent {
             url = prompt(this.props.intl.formatMessage(messages.extensionUrl));
         }
         if (url && !item.disabled) {
-            if (this.props.vm.extensionManager.isExtensionLoaded(url)) {
+            if (this.props.vm.extensionManager.isExtensionLoaded(id)) {
                 this.props.onCategorySelected(id);
             } else {
                 const parsedURL = isCustomURL ? parseExtensionURL(url) : url;
