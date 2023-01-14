36d6c054dbb05ef2cbed51bae0ae580af22800da
GarboMuffin <muffin@mailbox.org>
2023-01-14 14:20:36 -0600

    Fix ctrl+s to save when caps lock is enabled

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -280,7 +280,7 @@ class MenuBar extends React.Component {
     }
     handleKeyPress (event) {
         const modifier = bowser.mac ? event.metaKey : event.ctrlKey;
-        if (modifier && event.key === 's') {
+        if (modifier && event.key.toLowerCase() === 's') {
             this.props.handleSaveProject();
             event.preventDefault();
         }
