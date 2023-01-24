02a7b1a62cbb63af504240be5878cf5014165f7c
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-24 00:53:07 -0800

    bruh wth

--- src/components/custom-procedures/custom-procedures.jsx
+++ src/components/custom-procedures/custom-procedures.jsx
@@ -112,7 +112,7 @@ const CustomProcedures = props => (
                         id="gui.customProcedures.runWithoutScreenRefresh"
                     />
                 </label>
-                <br>
+                <br/>
                 <label>
                     <input
                         checked={props.returns}
