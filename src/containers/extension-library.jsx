ee60a7b07f18041d32cc13276159e0a25b1d5752
GarboMuffin <muffin@mailbox.org>
2023-03-10 23:33:23 -0600

    Don't show compatibility warning when clicking custom extension button

--- src/containers/extension-library.jsx
+++ src/containers/extension-library.jsx
@@ -46,11 +46,11 @@ class ExtensionLibrary extends React.PureComponent {
         ]);
     }
     handleItemSelect (item) {
+        const id = item.extensionId;
         // eslint-disable-next-line no-alert
-        if (item.incompatibleWithScratch && !confirm(this.props.intl.formatMessage(messages.incompatible))) {
+        if (item.incompatibleWithScratch && id && !confirm(this.props.intl.formatMessage(messages.incompatible))) {
             return;
         }
-        const id = item.extensionId;
         let url = item.extensionURL ? item.extensionURL : id;
         const isCustomURL = !item.disabled && !id;
         if (isCustomURL) {
