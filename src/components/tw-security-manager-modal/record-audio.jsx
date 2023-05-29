616a758196ee5913729ebbd6d8f9138dd08bc1f4
GarboMuffin <muffin@mailbox.org>
2023-05-29 11:41:23 -0500

    Fix typo

--- src/components/tw-security-manager-modal/record-audio.jsx
+++ src/components/tw-security-manager-modal/record-audio.jsx
@@ -6,7 +6,7 @@ const RecordAudio = () => (
         <p>
             <FormattedMessage
                 // eslint-disable-next-line max-len
-                defaultMessage="The project wants to to record audio from your microphone. This could include a text transcript or raw audio data. The project may be able to share audio with other users or servers."
+                defaultMessage="The project wants to record audio from your microphone. This could include a text transcript or raw audio data. The project may be able to share audio with other users or servers."
                 description="Part of modal that appears when a project tries to record audio using an extension"
                 id="tw.recordAudio.title"
             />
