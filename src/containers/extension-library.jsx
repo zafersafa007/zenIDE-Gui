bd4af383e4b32a8b68fa7855379d21456e8bd17b
RedMan13 <godslayerakp@gmail.com>
2023-06-08 02:00:02 -0700

    me when i forgot to press ctrl+s

--- src/containers/extension-library.jsx
+++ src/containers/extension-library.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import VM from 'scratch-vm';
 import {defineMessages, injectIntl, intlShape} from 'react-intl';
-import log from '../lib/log';
 
 import extensionLibraryContent from '../lib/libraries/extensions/index.jsx';
 import extensionTags from '../lib/libraries/extension-tags';
@@ -16,6 +15,11 @@ const messages = defineMessages({
         defaultMessage: 'Choose an Extension',
         description: 'Heading for the extension library',
         id: 'gui.extensionLibrary.chooseAnExtension'
+    },
+    extensionUrl: {
+        defaultMessage: 'Enter the URL of the extension',
+        description: 'Prompt for unoffical extension url',
+        id: 'gui.extensionLibrary.extensionUrl'
     },
     incompatible: {
         // eslint-disable-next-line max-len
@@ -42,26 +46,24 @@ class ExtensionLibrary extends React.PureComponent {
         ]);
     }
     handleItemSelect (item) {
-        const extensionId = item.extensionId;
-        const isCustomURL = !item.disabled && !extensionId;
-        if (isCustomURL) {
-            this.props.onOpenCustomExtensionModal();
-            return;
-        }
-
         // eslint-disable-next-line no-alert
         if (item.incompatibleWithScratch && !confirm(this.props.intl.formatMessage(messages.incompatible))) {
             return;
         }
-
-        const url = item.extensionURL ? item.extensionURL : extensionId;
-        if (!item.disabled) {
-            if (this.props.vm.extensionManager.isExtensionLoaded(extensionId)) {
-                this.props.onCategorySelected(extensionId);
+        const id = item.extensionId;
+        let url = item.extensionURL ? item.extensionURL : id;
+        const isCustomURL = !item.disabled && !id;
+        if (isCustomURL) {
+            // eslint-disable-next-line no-alert
+            url = prompt(this.props.intl.formatMessage(messages.extensionUrl));
+        }
+        if (url && !item.disabled) {
+            if (this.props.vm.extensionManager.isExtensionLoaded(url)) {
+                this.props.onCategorySelected(id);
             } else {
                 const parsedURL = isCustomURL ? parseExtensionURL(url) : url;
                 this.props.vm.extensionManager.loadExtensionURL(parsedURL)
-                    .then(id => {
+                    .then(() => {
                         this.props.onCategorySelected(id);
                         if (isCustomURL) {
                             let newUrl = location.pathname;
@@ -76,7 +78,6 @@ class ExtensionLibrary extends React.PureComponent {
                         }
                     })
                     .catch(err => {
-                        log.error(err);
                         // eslint-disable-next-line no-alert
                         alert(err);
                     });
@@ -107,7 +108,6 @@ class ExtensionLibrary extends React.PureComponent {
 ExtensionLibrary.propTypes = {
     intl: intlShape.isRequired,
     onCategorySelected: PropTypes.func,
-    onOpenCustomExtensionModal: PropTypes.func,
     onRequestClose: PropTypes.func,
     visible: PropTypes.bool,
     vm: PropTypes.instanceOf(VM).isRequired // eslint-disable-line react/no-unused-prop-types
