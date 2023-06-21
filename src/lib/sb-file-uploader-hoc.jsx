73911a8956a44c8098ca7e2ae8c7e86e6d55d36a
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2023-06-21 13:58:03 -0700

    Update sb-file-uploader-hoc.jsx

--- src/lib/sb-file-uploader-hoc.jsx
+++ src/lib/sb-file-uploader-hoc.jsx
@@ -104,7 +104,7 @@ const SBFileUploaderHOC = function (WrappedComponent) {
             } else {
                 // create <input> element and add it to DOM
                 this.inputElement = document.createElement('input');
-                this.inputElement.accept = '.sb,.sb2,.sb3,.pm';
+                this.inputElement.accept = '.sb,.sb2,.sb3,.pm,.pmp';
                 this.inputElement.style = 'display: none;';
                 this.inputElement.type = 'file';
                 this.inputElement.onchange = this.handleChange; // connects to step 3
