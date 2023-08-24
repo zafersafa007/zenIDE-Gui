8575e9e1b1a447d7b1583b06f568eee26b196267
RedMan13 <godslayerakp@gmail.com>
2023-08-24 15:37:55 -0700

    merge pause button into gui

---
+++ src/components/pause-button/pause-button.jsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import pauseIcon from './icon--pause.svg';
+import playIcon from './icon--play.svg';
+import styles from './pause-button.css';
+
+const PauseButtonComponent = function (props) {
+    const {
+        paused,
+        className,
+        onClick,
+        title,
+        ...componentProps
+    } = props;
+    return (
+        <img
+            className={classNames(
+                className,
+                styles.pauseBtn
+            )}
+            draggable={false}
+            src={paused ? playIcon : pauseIcon}
+            title={title}
+            onClick={onClick}
+            // tw: also fire click when opening context menu (right click on all systems and alt+click on chromebooks)
+            onContextMenu={onClick}
+            {...componentProps}
+        />
+    );
+};
+PauseButtonComponent.propTypes = {
+    paused: PropTypes.bool,
+    className: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
+    title: PropTypes.string
+};
+PauseButtonComponent.defaultProps = {
+    paused: false,
+    title: 'Pause'
+};
+export default PauseButtonComponent;
