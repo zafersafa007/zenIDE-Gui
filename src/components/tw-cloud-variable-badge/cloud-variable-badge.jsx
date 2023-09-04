7e4c33db4d803148fd5c79bd932808a2eb3c9eab
GarboMuffin <muffin@mailbox.org>
2023-09-03 22:59:13 -0500

    Credit 9gr for providing the EU cloud variable server

--- src/components/tw-cloud-variable-badge/cloud-variable-badge.jsx
+++ src/components/tw-cloud-variable-badge/cloud-variable-badge.jsx
@@ -8,75 +8,78 @@ import {APP_NAME} from '../../lib/brand';
 
 const hosts = [
     {
-        // Provided by GarboMuffin
         name: 'US East',
         cloudHost: 'wss://clouddata.turbowarp.org'
     },
     {
-        // Provided by Apricot
         name: 'EU',
-        cloudHost: 'wss://clouddata-eu.turbowarp.org'
+        cloudHost: 'wss://clouddata-eu.turbowarp.org',
+        provider: {
+            name: '9gr',
+            href: 'https://scratch.mit.edu/users/9gr/'
+        }
     }
 ];
 
-const CloudVariableBadge = props => (
-    <div className={styles.badge}>
-        <div className={styles.title}>
-            <img
-                className={styles.cloudIcon}
-                src={cloudIcon}
-                alt=""
-                width={32}
-                height={32}
-            />
+const CloudVariableBadge = props => {
+    const selectedHost = hosts.find(i => i.cloudHost === props.cloudHost);
+    return (
+        <div className={styles.badge}>
+            <div className={styles.title}>
+                <img
+                    className={styles.cloudIcon}
+                    src={cloudIcon}
+                    alt=""
+                    width={32}
+                    height={32}
+                />
+                <FormattedMessage
+                    // eslint-disable-next-line max-len
+                    defaultMessage="This project uses cloud variables."
+                    description="Cloud variable information shown under projects with cloud variables"
+                    id="tw.usesCloudVariables"
+                />
+            </div>
+    
             <FormattedMessage
                 // eslint-disable-next-line max-len
-                defaultMessage="This project uses cloud variables."
-                description="Cloud variable information shown under projects with cloud variables"
-                id="tw.usesCloudVariables"
+                defaultMessage="{APP_NAME}'s cloud variables are not connected to Scratch's. Anyone can {changeTheirUsername} to anything, so beware of impersonation."
+                // eslint-disable-next-line max-len
+                description="Cloud variable information shown under projects with cloud variables. {changeTheirUsername} will be replaced with a link with text 'change their username' (translated)"
+                id="tw.usesCloudVariables2"
+                values={{
+                    APP_NAME,
+                    changeTheirUsername: (
+                        <a onClick={props.onOpenChangeUsername}>
+                            <FormattedMessage
+                                defaultMessage="change their username"
+                                // eslint-disable-next-line max-len
+                                description="Link that opens modal to change one's username. Used in the context 'Anyone can change their username'"
+                                id="tw.usesCloudVariables2.change"
+                            />
+                        </a>
+                    )
+                }}
             />
-        </div>
-
-        <FormattedMessage
-            // eslint-disable-next-line max-len
-            defaultMessage="{APP_NAME}'s cloud variables are not connected to Scratch's. Anyone can {changeTheirUsername} to anything, so beware of impersonation."
-            // eslint-disable-next-line max-len
-            description="Cloud variable information shown under projects with cloud variables. {changeTheirUsername} will be replaced with a link with text 'change their username' (translated)"
-            id="tw.usesCloudVariables2"
-            values={{
-                APP_NAME,
-                changeTheirUsername: (
-                    <a onClick={props.onOpenChangeUsername}>
-                        <FormattedMessage
-                            defaultMessage="change their username"
-                            // eslint-disable-next-line max-len
-                            description="Link that opens modal to change one's username. Used in the context 'Anyone can change their username'"
-                            id="tw.usesCloudVariables2.change"
-                        />
-                    </a>
-                )
-            }}
-        />
-
-        {hosts.some(i => i.cloudHost === props.cloudHost) ? (
-            <div className={styles.servers}>
-                <FormattedMessage
-                    defaultMessage="Pick a server near you:"
-                    description="Appears before a list of cloud variable servers in different countries"
-                    id="tw.cloudServers"
-                />
-                {hosts.map(i => (
-                    <CloudServerButton
-                        key={i.ws}
-                        name={i.name}
-                        cloudHost={i.cloudHost}
-                        selected={props.cloudHost === i.cloudHost}
-                        onClick={props.onSetCloudHost}
+    
+            {selectedHost ? (
+                <div className={styles.servers}>
+                    <FormattedMessage
+                        defaultMessage="Pick a server near you:"
+                        description="Appears before a list of cloud variable servers in different countries"
+                        id="tw.cloudServers"
                     />
-                ))}
-            </div>
-        ) : (
-            <div className={styles.servers}>
+                    {hosts.map(i => (
+                        <CloudServerButton
+                            key={i.ws}
+                            name={i.name}
+                            cloudHost={i.cloudHost}
+                            selected={props.cloudHost === i.cloudHost}
+                            onClick={props.onSetCloudHost}
+                        />
+                    ))}
+                </div>
+            ) : (
                 <FormattedMessage
                     defaultMessage="Using a custom cloud variable server: {server}"
                     // eslint-disable-next-line max-len
@@ -86,22 +89,41 @@ const CloudVariableBadge = props => (
                         server: props.cloudHost
                     }}
                 />
-            </div>
-        )}
+            )}
 
-        <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://docs.turbowarp.org/cloud-variables"
-        >
-            <FormattedMessage
-                defaultMessage="Learn more about cloud variables."
-                description="Link for more information about cloud variables"
-                id="tw.moreCloud"
-            />
-        </a>
-    </div>
-);
+            {selectedHost && selectedHost.provider && (
+                <FormattedMessage
+                    defaultMessage="Server provided by {name}."
+                    description="Link to person/company who provided this cloud variable server"
+                    id="tw.cloudProvider"
+                    values={{
+                        name: (
+                            <a
+                                href={selectedHost.provider.href}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                {selectedHost.provider.name}
+                            </a>
+                        )
+                    }}
+                />
+            )}
+    
+            <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://docs.turbowarp.org/cloud-variables"
+            >
+                <FormattedMessage
+                    defaultMessage="Learn more about cloud variables."
+                    description="Link for more information about cloud variables"
+                    id="tw.moreCloud"
+                />
+            </a>
+        </div>
+    );
+};
 
 CloudVariableBadge.propTypes = {
     cloudHost: PropTypes.string,
