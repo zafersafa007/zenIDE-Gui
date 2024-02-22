dbc2e07f58533ab4518c501b83152888a848cbcc
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-02-21 21:25:08 -0800

    Update make-toolbox-xml.js

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -429,7 +429,7 @@ const looks = function (isInitialSetup, isStage, targetId, costumeName, backdrop
                     </shadow>
                 </value>
             </block>
-            <block type="looks_goTargetlayer">
+            <block type="looks_goTargetLayer">
                 <value name="VISIBLE_OPTION">
                     <shadow type="looks_getOtherSpriteVisible_menu"/>
                 </value>
