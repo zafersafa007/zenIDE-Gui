888830586e44e5ba08962b328a14c11ca7307daa
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-04-15 20:46:25 -0700

    Update extension-library.jsx

--- src/containers/extension-library.jsx
+++ src/containers/extension-library.jsx
@@ -37,7 +37,7 @@ const TRUSTED_LOADEXT_ORIGINS = [
     'https://studio.penguinmod.com', // for development
     'https://extensions.penguinmod.com',
     'https://sharkpools-extensions.vercel.app',
-    'https://sharkpool-sp.github.io/SharkPools-Extensions', // Some people cant connect to vercel
+    'https://raw.githubusercontent.com/SharkPool-SP/SharkPools-Extensions/main', // Some people cant connect to vercel
     'https://pen-group.github.io',
 ];
 
