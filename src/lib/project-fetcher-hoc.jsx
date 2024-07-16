ebe934f818f5a1b3e21af44c4a2c0ccda6a1863a
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-07-16 17:38:45 -0500

    change the way we handle protobuf files

--- src/lib/project-fetcher-hoc.jsx
+++ src/lib/project-fetcher-hoc.jsx
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import { intlShape, injectIntl } from 'react-intl';
 import bindAll from 'lodash.bindall';
 import { connect } from 'react-redux';
-import Project from './project.protobuf.js';
-import Pbf from './pbf.js';
+import protobufBundle from './project.protobuf.json';
+import protobuf from 'protobufjs';
 import JSZip from 'jszip';
+
+let protoRoot = protobuf.Root.fromJSON(protobufBundle);
+let Project = protoRoot.lookupType('project.Project');
 
 import { setProjectUnchanged } from '../reducers/project-changed';
 import {
@@ -61,7 +64,8 @@ const fetchProjectToken = projectId => {
 };
 
 function protobufToJson(buffer) {
-    const json = Project.read(buffer);
+    const message = Project.decode(buffer);
+    const json = Project.toObject(message);
 
     const newJson = {
         targets: [],
@@ -79,7 +83,7 @@ function protobufToJson(buffer) {
 
     for (const target of json.targets) {
         let newTarget = {
-            isStage: target.isStage,
+            isStage: target.isStage || false,
             name: target.name,
             variables: {},
             lists: {},
@@ -299,8 +303,7 @@ const ProjectFetcherHOC = function (WrappedComponent) {
                             }
                             const project = await r.json();
 
-                            const pbf = new Pbf(new Uint8Array(project.project.data));
-                            const json = protobufToJson(pbf);
+                            const json = protobufToJson(new Uint8Array(project.project.data));
 
                             // now get the assets
                             let zip = new JSZip();
